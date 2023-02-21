42d578f06f6a60fee55a1c4789c889c6abafc369
kano <sunflower.tandf@gmail.com>
2023-02-21 19:12:12 +0900

    タグ検索

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ApplicationRecord
-    belongs_to :user
+
     mount_uploader :image, ImageUploader
     has_many :pin_tag_relations, dependent: :destroy
     has_many :tags, through: :pin_tag_relations, dependent: :destroy
